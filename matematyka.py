a595f05578d5e064fbbf1655b0875f9b7f3b00f2
radek <rajkonkret660@gmail.com>
2024-01-24 12:25:08 +0100

    3 - add matematyka.py

---
+++ matematyka.py
@@ -0,0 +1,11 @@
+import math
+
+pi = math.pi
+print(pi)  # 3.141592653589793
+
+print(math.sqrt(1025))
+print(math.cos(50))  # 0.9649660284921133
+print(pow(1.7 * 10 ** 308, 1))  # 1.7e+308
+print(pow(1.8 * 10 ** 308, 1))  # inf
+
+print(math.pow(2, 10))  # 1024.0
