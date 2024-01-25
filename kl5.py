4774e73f2eb6ed95fb9dc0a6d25852e681b7fd22
radek <rajkonkret660@gmail.com>
2024-01-25 13:42:58 +0100

    4 - update kl5

--- kl5.py
+++ kl5.py
@@ -9,13 +9,20 @@ class Pojazd:
 
 class Samochod(Pojazd):  # dziedziczy po klasie Pojazd
     """Klasa Samochod"""
+
     def __init__(self, kolor, marka):
         super().__init__(kolor)
         self.marka = marka
 
+    def info(self):
+        super().info()
+        print(f'Marka: {self.marka}')
+
+
 poj = Pojazd("Czerwony")
 poj.info()
 
-sam1 = Samochod("Zielony")
+sam1 = Samochod("Zielony", "Ford")
 sam1.info()  # Kolor: Zielony
-
+# Kolor: Zielony
+# Marka: Ford
