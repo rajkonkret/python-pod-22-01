2fe8285bcf1a19215489dcf98ea70c94826aefb1
radek <rajkonkret660@gmail.com>
2024-01-26 12:40:48 +0100

    5 - update api-nbp

--- api-nbp.py
+++ api-nbp.py
@@ -1,8 +1,17 @@
 import requests as re
 
-url = 'http://api.nbp.pl/api/exchangerates/rates/A/USD/'
+# url = 'http://api.nbp.pl/api/exchangerates/rates/A/USD/'
+url = 'http://api.nbp.pl/api/exchangerates/rates/A/EUR/'
 
 response = re.get(url)
 print(response.text)
 data = response.json()
 print(data)
+
+rates = data['rates']
+print(rates)
+print(type(rates))
+# [{'no': '019/A/NBP/2024', 'effectiveDate': '2024-01-26', 'mid': 4.0393}]
+rate = rates[0]
+print(type(rate))  # <class 'dict'>
+print(rate['mid'])  # 4.0393
