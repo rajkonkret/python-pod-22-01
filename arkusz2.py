8b7c7b5988b3c18b1c52d2d4e3b244cb3273fc83
radek <rajkonkret660@gmail.com>
2024-01-26 14:17:35 +0100

    5 - update arkusz2.py

--- arkusz2.py
+++ arkusz2.py
@@ -16,3 +16,14 @@ print(data.index[-1])  # 3 ostatni numer indexu
 
 print(type(data.columns[0]))  # <class 'str'>
 print(type(data.columns))  # <class 'pandas.core.indexes.base.Index'>
+# [[Timestamp('2018-05-12 00:00:00') 'Sila Ahmed' 60000]
+#  [Timestamp('2019-12-06 00:00:00') 'Mir Hossain' 50000]
+#  [Timestamp('2020-08-09 00:00:00') 'Sarmin Jahan' 45000]
+#  [Timestamp('2021-04-07 00:00:00') 'Mahmudul Hasan' 30000]]
+# Index(['Sales Date', 'Sales Person', 'Amount'], dtype='object')
+# <bound method DataFrame.items of   Sales Date    Sales Person  Amount
+# 0 2018-05-12      Sila Ahmed   60000
+# 1 2019-12-06     Mir Hossain   50000
+# 2 2020-08-09    Sarmin Jahan   45000
+# 3 2021-04-07  Mahmudul Hasan   30000>
+# 3
